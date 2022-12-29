3997db3835a6af5c4736b937cc5756867b7699ce
CarlosjuniorBr1 <carlos.alberto369@outlook.com>
2022-12-29 19:48:54 -0300

    Lendo e enumerando um linhas de um arquivo

---
+++ aula7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+
+        private static void LerArquivo(string nomeArquivo)
+        {
+            using StreamReader arquivo = File.OpenText(nomeArquivo);
+            string linha;
+            int i = 1;
+            while ((linha = arquivo.ReadLine()) != null)
+            {
+               // Console.WriteLine(i);
+                Console.WriteLine(i +" - " + linha);
+                i++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            LerArquivo(@"C:\arquivos\arq1.txt");
+
+        }
+    }
+}
